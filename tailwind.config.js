eca07581287186f3bedf4421530019da146c9a69
manisaparajuli <manisaparajuli66@gmail.com>
2024-05-11 18:13:44 +1000

    hero assest added

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
       backgroundImage: (theme) => ({
         "gradient-yellowred":
           "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
-        "mobile-home": "url('./assets/HomePageGraphic.png')",
+        "mobile-home": "url('./assets/Homepagegraphic.png')",
       }),
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
