808cecc11ec753a7f7a95e673b44011baf908d55
manisaparajuli <manisaparajuli66@gmail.com>
2024-05-11 19:22:31 +1000

    navbar links added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Navbar from "@/scenes/navbar/index";
+import { useState } from "react";
 function App() {
-
+  const [selectedPage, setSelectedPage] = useState("home");
   return (
     <div className='app bg-gray-20'>
-      <Navbar/>
+      <Navbar 
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
       
     </div>
   )
